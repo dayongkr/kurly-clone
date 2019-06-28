32596ca36ab12272d90939878c07b0af219e6bdd
dayongbz <dayongbz@gmail.com>
2019-06-28 12:42:35 +0900

    Header 끝

---
+++ src/Component/Header.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef } from 'react';
+
+export default function Header() {
+  const headerRef = useRef();
+  const onScroll = () => {
+    if (headerRef.current) {
+      console.log(headerRef.current.offsetTop);
+      if (document.querySelector('#topPopUp').style.display === 'none') {
+        if (window.scrollY >= 142 - 42) {
+          headerRef.current.style.position = 'fixed';
+          headerRef.current.style.top = 0;
+        } else {
+          headerRef.current.style.position = 'static';
+        }
+      } else if (window.scrollY >= 142) {
+        headerRef.current.style.position = 'fixed';
+        headerRef.current.style.top = 0;
+      } else {
+        headerRef.current.style.position = 'static';
+      }
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+  return (
+    <header>
+      <div id="headerTop">
+        <div id="headerDeli" />
+        <ul id="userMenu">
+          <li className="userItem strong">회원가입</li>
+          <li className="userItem">로그인</li>
+          <li className="userItem">고객센터</li>
+          <li className="userItem">배송지역 검색</li>
+        </ul>
+      </div>
+      <div id="headerCen">
+        <img id="logoImg" src="./image/logo.png" alt="로고이미지" />
+      </div>
+      <div id="headerBott" ref={headerRef}>
+        <div id="headerBottWrap">
+          <ul id="mainMenu">
+            <li className="strong">
+              <img src="./image/menuIcon.png" alt="" />
+              전체 카테고리
+            </li>
+            <li>신상품</li>
+            <li>베스트</li>
+            <li>알뜰쇼핑</li>
+            <li>이벤트</li>
+          </ul>
+          <div id="searchWrap">
+            <input id="search" type="text" placeholder="입력해주세요~~" />
+            <img src="./image/searchIcon@2x.png" alt="검색아이콘" />
+          </div>
+          <img
+            src="./image/shoppingCartIcon@2x.png"
+            width="30px"
+            alt="쇼핑카트아이콘"
+            id="shoppingCartImg"
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './Component/Header';
+import Main from './Component/Main';
+
+function useClose() {
+  document.querySelector('#topPopUp').style.opacity = 0;
+  setTimeout(() => {
+    document.querySelector('#topPopUp').style.display = 'none';
+  }, 1000);
+}
+
+function App() {
+  return (
+    <>
+      <div id="topPopUp">
+        <div id="topPopWrap">
+          지금 가입하고 인기 상품 100원에 받아가세요!
+          <div id="topPopCls" onClick={useClose}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+                fill="white"
+              />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <Header />
+      <Main />
+      <div style={{ height: '1000vh' }} />
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
