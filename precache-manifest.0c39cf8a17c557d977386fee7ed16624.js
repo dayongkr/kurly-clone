b14eb0467cb4a19ebab5925aaa45bc91d9ee9bff
dayongbz <dayongbz@gmail.com>
2019-06-28 12:44:26 +0900

    Updates

---
+++ precache-manifest.0c39cf8a17c557d977386fee7ed16624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1dbc28dd8695f771e894d4fb20505f0",
+    "url": "/kurly-clone/index.html"
+  },
+  {
+    "revision": "c6828f601e600ce4d069",
+    "url": "/kurly-clone/static/css/main.d025527d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/kurly-clone/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c6828f601e600ce4d069",
+    "url": "/kurly-clone/static/js/main.f486cad5.chunk.js"
+  },
+  {
+    "revision": "4f5a6381895a84af5c35",
+    "url": "/kurly-clone/static/js/runtime~main.0ef87115.js"
+  }
+]);
