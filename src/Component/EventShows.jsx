699b255ab301d2327b2a3f621e230f413d134770
dayongbz <dayongbz@gmail.com>
2019-07-01 18:06:39 +0900

    css min-width
    해상도가 낮은 기기에서 문제가 생겨 수정

--- src/Component/EventShows.jsx
+++ src/Component/EventShows.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 export default function EventShows() {
   return (
-    <>
+    <div className="eventShowsWrap">
       <img
         src="./image/backgroundTop.png"
         alt="배경윗부분"
         style={{ display: 'block' }}
         width="100%"
       />
-      <div className="eventShowsWrap" />
+      <div className="eventShows" />
       <img
         src="./image/backgroundBottom.png"
         alt="배경아랫부분"
         style={{ display: 'block' }}
         width="100%"
       />
-    </>
+    </div>
   );
 }
